c8aebd326936d7b3221650e41f10c77c35d21af1
saidurpavel <90066296+saidurpavel@users.noreply.github.com>
2025-07-14 04:22:12 +0000

    Deploying to gh-pages from @ saidurpavel/saidurpavel.github.io@d7d48a85ac764c363773cba1a68919dce6eb1ba9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -36,6 +29,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/services/";
+          },
+        },{id: "nav-course-taken",
+          title: "Course Taken",
+          description: "Materials for courses you taught. Replace this text with your description.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/courses/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
