0add8e22aa25e1fd7ff49699e461473a8bad3e79
saidurpavel <90066296+saidurpavel@users.noreply.github.com>
2025-11-03 01:29:17 +0000

    Deploying to gh-pages from @ saidurpavel/saidurpavel.github.io@c246dee6f10770b5cb2612f7c469fad10637c525 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,8 +398,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-presented-our-paper-in-the-asilomar-2025-conference",
+          title: 'Presented our paper in the Asilomar 2025 conference',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/asilomar_conference/";
